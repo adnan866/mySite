ee1ba7a935aa25e64a4b692e0fb187d827e5006c
Adnan Maqsood <48270105+adnan866@users.noreply.github.com>
2024-10-04 04:06:46 +0500

    Initial commit

--- src/components/about/Skills.jsx
+++ src/components/about/Skills.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 const skillsContent = [
-  { skillClass: "p95", skillPercent: "95", skillName: "HTML" },
-  { skillClass: "p90", skillPercent: "90", skillName: "CSS" },
-  { skillClass: "p80", skillPercent: "85", skillName: "JQUERY" },
+  { skillClass: "p96", skillPercent: "96", skillName: "HTML" },
+  { skillClass: "p95", skillPercent: "95", skillName: "CSS" },
+  { skillClass: "p85", skillPercent: "85", skillName: "JQUERY" },
   { skillClass: "p86", skillPercent: "86", skillName: "JAVASCRIPT" },
-  { skillClass: "p85", skillPercent: "85", skillName: "REACTJS" },
+  { skillClass: "p82", skillPercent: "82", skillName: "REACTJS" },
   { skillClass: "p80", skillPercent: "80", skillName: "NextTJS" },
-  { skillClass: "p75", skillPercent: "75", skillName: "TypeScript" },
+  { skillClass: "p70", skillPercent: "70", skillName: "TypeScript" },
   { skillClass: "p85", skillPercent: "85", skillName: "Tailwind" },
   { skillClass: "p90", skillPercent: "90", skillName: "BootStrap" },
-  { skillClass: "p75", skillPercent: "75", skillName: "SASS" },
-  { skillClass: "p85", skillPercent: "80", skillName: "API" },
+  { skillClass: "p80", skillPercent: "80", skillName: "SASS" },
+  { skillClass: "p80", skillPercent: "80", skillName: "API" },
   { skillClass: "p95", skillPercent: "95", skillName: "WORDPRESS" },
   { skillClass: "p82", skillPercent: "82", skillName: "Git/Github" },
   { skillClass: "p75", skillPercent: "75", skillName: "Sharepoint" },
-  { skillClass: "p65", skillPercent: "65", skillName: "SPFX" },
+  { skillClass: "p70", skillPercent: "70", skillName: "SPFX" },
   { skillClass: "p90", skillPercent: "90", skillName: "cloudways" },
 ];
 
