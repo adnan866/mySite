773826ebf831256481639b200f4b7e311a8f2cdc
Adnan Maqsood <48270105+adnan866@users.noreply.github.com>
2026-04-20 05:41:37 +0500

    Update address details in Address component

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -5,7 +5,7 @@ const Address = () => {
     <>
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-map-marker position-absolute"></i>
-        <span className="d-block">Address Point</span>Near Big Satwa Mosque Dubai,UAE
+        <span className="d-block">Address Point</span>Lahore, Pakistan
       </p>
       {/* End .custom-span-contact */}
 
@@ -24,8 +24,7 @@ const Address = () => {
       <p className="open-sans-font custom-span-contact position-relative">
         <i class="fa fa-whatsapp position-absolute"></i>
         <span className="d-block">Whatsapp</span>{" "}
-        <a href="https://wa.link/x21yf9">+92 305 9843 866</a>
-        <a href="https://wa.link/3x3pv8">+971 547 597 296</a>
+        <a href="https://wa.link/x21yf9">+92 305 9843 866</a> 
       </p>
       {/* End .custom-span-contact */}
     </>
